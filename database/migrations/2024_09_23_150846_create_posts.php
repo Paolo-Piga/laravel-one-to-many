07e73786f04740b4f8d12d88e696231c7d790e27
DecimTheJudge <paolo1piga@gmail.com>
2024-09-25 15:22:11 +0200

    start

---
+++ database/migrations/2024_09_23_150846_create_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            Schema::create('post_table', function (Blueprint $table) {
+                $table->id();
+                $table->char('title', 255);
+                $table->string('topic', 255);
+                $table->date('start_date');
+                $table->date('end_date');
+                $table->string('post_number', 255);
+                $table->string('team', 255);
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
