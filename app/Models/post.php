4ed4e8355441a2c28ebbaa1a298201a372590cbc
DecimTheJudge <paolo1piga@gmail.com>
2024-09-25 16:40:48 +0200

    fix:post

--- app/Models/post.php
+++ app/Models/post.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function posts(){
+        return $this->belongsTo(Posting::class);
+    }
+
+    protected $fillable = ['title', 'slug'];
+}
